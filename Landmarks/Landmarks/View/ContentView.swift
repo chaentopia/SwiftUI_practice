c21ef56646e265035a8ca60bc38522e51197762a
정채은 <cce523@hanmail.net>
2023-02-20 01:48:00 +0900

    [Feat] Building Lists and Navigation

---
+++ Landmarks/Landmarks/View/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 정채은 on 2023/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
